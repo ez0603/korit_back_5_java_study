25614e8e78b82c46aa61269f3aac7c71bb3b37bf
ez0603 <dlwldjs3132@naver.com>
2024-01-02 17:11:21 +0900

    example01

---
+++ src/main/java/com/study/ch04/Operation07.java
@@ -0,0 +1,21 @@
+package com.study.ch04;
+
+//*1.  Operation07.java 파일 생성
+//2. 깃, 깃허브 수업 실습 println
+//3. add, commit, push
+//4. 자리 바꾸기
+//5. 계정 변경
+//6. 클론 java_study_본인이름 영어로
+//7. Operation07.java 파일에서 "변경사항 추가" println
+//8. add, commit, push
+//9. 다시 자기 자리로 돌아오기
+//10. 계정 변경
+//10. fetch, pull
+// *//
+
+public class Operation07 {
+    public static void main(String[] args) {
+        System.out.println("깃, 깃허브 수업 실습"); // 2. 깃,깃허브 수업 실습 println
+
+    }
+}
